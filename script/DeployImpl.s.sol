7f391917bdb349d0a09c206b2458f172a24c83c7
salvanexus <charlieonyii@gmail.com>
2026-04-11 15:15:35 +0100

    Update: Sub-Name Format check for phishing resistance

--- script/DeployImpl.s.sol
+++ script/DeployImpl.s.sol
@@ -17,7 +17,7 @@ contract DeployImpl is Script {
 
     function run() external broadcastLive {
         // NGN -> 0x78E9917e6A7D7DD2fd3fc031723741F4f755641C
-        Singleton singProxy = Singleton(0xaeb9fcC270F240FAA9A7f9d8b84eD6fE5c8f6b61);
+        // Singleton singProxy = Singleton(0xaeb9fcC270F240FAA9A7f9d8b84eD6fE5c8f6b61);
         MultiSig multsigproxy = MultiSig(0xEe1195Ba5A9844a5b697A7a7070D7C2FBA0e45f0);
         // RegistryFactory factory = RegistryFactory(0x7c6F02D4A226D0fFd6e0d47b50D0336aDb5c9CD6)
         //MultiSig mulsig = new MultiSig();
