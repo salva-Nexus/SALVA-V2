da192a13df4ff602886c61e65455cedaf87bfd82
salvanexus <charlieonyii@gmail.com>
2026-05-03 18:10:28 +0100

    v2.1.0

--- script/DeployImpl.s.sol
+++ script/DeployImpl.s.sol
@@ -17,15 +17,40 @@ contract DeployImpl is Script {
     }
 
     function run() external broadcastLive {
-        address ngnsMainnet = address(0x78E9917e6A7D7DD2fd3fc031723741F4f755641C);
-        address ngnsTestnet = (0xae7597fa3414Bc94254fA7777663882355ED6Cb7);
+        // address ngnsMainnet = address(0x78E9917e6A7D7DD2fd3fc031723741F4f755641C);
+        // address ngnsTestnet = (0xae7597fa3414Bc94254fA7777663882355ED6Cb7);
         Singleton singProxyMainnet = Singleton(0xc03eDeB2EF48B752ce46600d088206f5334e5546);
-        Singleton singProxyTestnet = Singleton(0x75e36Bb8F36A6aE1799E34E3161719964fECC22C);
+        // Singleton singProxyTestnet = Singleton(0x75e36Bb8F36A6aE1799E34E3161719964fECC22C);
         MultiSig multsigproxyMainnet = MultiSig(0xd2611e3acE93303052478af5EE5d13e2E9c63C7A);
-        MultiSig multsigProxyTestnet = MultiSig(0x361A059ae5ef356f70DD535dc3f5A7db59350Ec6);
+        // MultiSig multsigProxyTestnet = MultiSig(0x361A059ae5ef356f70DD535dc3f5A7db59350Ec6);
         RegistryFactory factoryProxyMainnet =
             RegistryFactory(0xdc2b063198Fa973F37232256Bb822F456332eFc8);
-        RegistryFactory factoryProxyTestnet =
-            RegistryFactory(0x7B56bc1e4eFCAED94882F69003087Eef93aC4c41);
+        // RegistryFactory factoryProxyTestnet =
+        //    RegistryFactory(0x7B56bc1e4eFCAED94882F69003087Eef93aC4c41);
+
+        Singleton newSingleton = new Singleton();
+        MultiSig newMultiSig = new MultiSig();
+        RegistryFactory newFactory = new RegistryFactory();
+
+        address[3] memory proxies =
+            [address(singProxyMainnet), address(multsigproxyMainnet), address(factoryProxyMainnet)];
+        address[3] memory newImpls =
+            [address(newSingleton), address(newMultiSig), address(newFactory)];
+
+        for (uint256 i = 0; i < proxies.length;) {
+            address proxy = proxies[i];
+            address newImpl = newImpls[i];
+            if (proxy == address(multsigproxyMainnet)) {
+                multsigproxyMainnet.proposeUpgrade(proxy, newImpl, true);
+            } else {
+                multsigproxyMainnet.proposeUpgrade(proxy, newImpl, false);
+            }
+            multsigproxyMainnet.validateUpgrade(newImpl);
+            multsigproxyMainnet.executeUpgrade(newImpl);
+
+            unchecked {
+                i++;
+            }
+        }
     }
 }
