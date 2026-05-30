e2cc12d36676d52ea88546411dd19b21930f7bda
salvanexus <charlieonyii@gmail.com>
2026-05-30 23:36:37 +0100

    Update: Removed Redundant Codes

--- script/DeployImpl.s.sol
+++ script/DeployImpl.s.sol
@@ -17,14 +17,19 @@ contract DeployImpl is Script {
     }
 
     function run() external broadcastLive {
+        upgrade();
+    }
+
+    function upgrade() public {
+
         // address ngnsMainnet = address(0x78E9917e6A7D7DD2fd3fc031723741F4f755641C);
         // address ngnsTestnet = (0xae7597fa3414Bc94254fA7777663882355ED6Cb7);
-        //Singleton singProxyMainnet = Singleton(0xc03eDeB2EF48B752ce46600d088206f5334e5546);
+        Singleton singProxyMainnet = Singleton(0xD5B1Cb07403b946b536c77a6102723808971c87C);
         // Singleton singProxyTestnet = Singleton(0x75e36Bb8F36A6aE1799E34E3161719964fECC22C);
-        MultiSig multsigproxyMainnet = MultiSig(0xd2611e3acE93303052478af5EE5d13e2E9c63C7A);
+        MultiSig multsigproxyMainnet = MultiSig(0xd0960377a40E3d554945Bd801EC36c2f62d0d205);
         // MultiSig multsigProxyTestnet = MultiSig(0x361A059ae5ef356f70DD535dc3f5A7db59350Ec6);
         // RegistryFactory factoryProxyMainnet =
-        //     RegistryFactory(0xdc2b063198Fa973F37232256Bb822F456332eFc8);
+        //     RegistryFactory(0x540bC86d5D66A7426C4026ff93D75297Ad4777BB);
         // RegistryFactory factoryProxyTestnet =
         //    RegistryFactory(0x7B56bc1e4eFCAED94882F69003087Eef93aC4c41);
 
@@ -50,5 +55,8 @@ contract DeployImpl is Script {
                 i++;
             }
         }
+
+        // console.log("Singleton upgraded to:", address(newSingleton));
+
     }
 }
