d05f099af84a95b01dd44a0323a3e7b6b3b00ab4
Salva Nexus <charlieonyii@gmail.com>
2026-06-28 01:27:02 +0100

    Fix: Namespace length update during resolve

--- script/DeployImpl.s.sol
+++ script/DeployImpl.s.sol
@@ -21,12 +21,12 @@ contract DeployImpl is Script {
     }
 
     function upgrade() public {
-        // address ngnsMainnet = address(0x78E9917e6A7D7DD2fd3fc031723741F4f755641C); 
+        // address ngnsMainnet = address(0x78E9917e6A7D7DD2fd3fc031723741F4f755641C);
         // address ngnsTestnet = (0xae7597fa3414Bc94254fA7777663882355ED6Cb7);
-        // Singleton singProxyMainnet = Singleton(0xD5B1Cb07403b946b536c77a6102723808971c87C);
-        Singleton singProxyTestnet = Singleton(0x75e36Bb8F36A6aE1799E34E3161719964fECC22C);
-        // MultiSig multsigproxyMainnet = MultiSig(0xd0960377a40E3d554945Bd801EC36c2f62d0d205);
-        MultiSig multsigProxyTestnet = MultiSig(0x361A059ae5ef356f70DD535dc3f5A7db59350Ec6);
+        Singleton singProxyMainnet = Singleton(0xD5B1Cb07403b946b536c77a6102723808971c87C);
+        // Singleton singProxyTestnet = Singleton(0x75e36Bb8F36A6aE1799E34E3161719964fECC22C);
+        MultiSig multsigproxyMainnet = MultiSig(0xd0960377a40E3d554945Bd801EC36c2f62d0d205);
+        // MultiSig multsigProxyTestnet = MultiSig(0x361A059ae5ef356f70DD535dc3f5A7db59350Ec6);
         // RegistryFactory factoryProxyMainnet =
         //     RegistryFactory(0x540bC86d5D66A7426C4026ff93D75297Ad4777BB);
         // RegistryFactory factoryProxyTestnet =
@@ -36,25 +36,25 @@ contract DeployImpl is Script {
         // MultiSig newMultiSig = new MultiSig();
         // RegistryFactory newFactory = new RegistryFactory();
 
-        address[1] memory proxies = [address(singProxyTestnet)];
+        address[1] memory proxies = [address(singProxyMainnet)];
         address[1] memory newImpls = [address(newSingleton)];
 
         for (uint256 i = 0; i < proxies.length;) {
             address proxy = proxies[i];
             address newImpl = newImpls[i];
-            if (proxy == address(multsigProxyTestnet)) {
-                multsigProxyTestnet.proposeUpgrade(proxy, newImpl, true);
+            if (proxy == address(multsigproxyMainnet)) {
+                multsigproxyMainnet.proposeUpgrade(proxy, newImpl, true);
             } else {
-                multsigProxyTestnet.proposeUpgrade(proxy, newImpl, false);
+                multsigproxyMainnet.proposeUpgrade(proxy, newImpl, false);
             }
-            multsigProxyTestnet.validateUpgrade(newImpl);
-            multsigProxyTestnet.executeUpgrade(newImpl);
+            multsigproxyMainnet.validateUpgrade(newImpl);
+            multsigproxyMainnet.executeUpgrade(newImpl);
 
             unchecked {
                 i++;
             }
         }
 
-        console.log("Singleton upgraded to:", address(newSingleton));
+        // console.log("Singleton upgraded to:", address(newSingleton));
     }
 }
